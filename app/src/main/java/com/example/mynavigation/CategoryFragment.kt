166a26e96c0c7f68ae57c60aee1bf994af9bf79a
wdinx <wahyuudin2811@gmail.com>
2023-08-17 16:27:19 +0800

    Code for Navigation

---
+++ app/src/main/java/com/example/mynavigation/CategoryFragment.kt
@@ -0,0 +1,44 @@
+package com.example.mynavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.mynavigation.databinding.FragmentCategoryBinding
+
+class CategoryFragment : Fragment() {
+    private var _binding: FragmentCategoryBinding? = null
+    val binding get() = _binding!!
+
+    companion object{
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_STOCK = "extra_stock"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCategoryLifestyle.setOnClickListener {
+            val toDetailCategoryFragment = CategoryFragmentDirections.actionCategoryFragmentToDetailCategoryFragment()
+            toDetailCategoryFragment.name = "Lifestyle"
+            toDetailCategoryFragment.stock = 7L
+            view.findNavController().navigate(toDetailCategoryFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/example/mynavigation/DetailCategoryFragment.kt
@@ -0,0 +1,42 @@
+package com.example.mynavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.mynavigation.databinding.FragmentDetailCategoryBinding
+
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding: FragmentDetailCategoryBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataName = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).name
+        val dataDescription = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).stock
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescripiton.text = "Stock: $dataDescription"
+
+        binding.btnProfile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
